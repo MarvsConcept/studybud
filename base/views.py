b9f336501f621143c87966d31fb57671066d0aaf
Marvsconcept <marvade8@gmail.com>
2025-01-18 23:42:38 +0100

    Reeoute template to app

--- base/views.py
+++ base/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
 
+
+rooms = [
+    {'id':1, 'name':'Lets learn python!'},
+    {'id':1, 'name':'Design with Marv!'},
+    {'id':1, 'name':'Frontend Guys!'},
+]
+
 def home(request):
-    return render(request, 'home.html')
+    context =  {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 def room(request):
-    return render(request, 'room.html')
+    return render(request, 'base/room.html')
